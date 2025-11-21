69484f91cd399bbbe9e0d8c2e4f134b6c0691f52
TheMostSilly <geovannacastro058@gmail.com>
2025-11-21 15:38:16 -0500

    Problema02

---
+++ problematicas/Problema02/src/problema02/Problema02.java
@@ -0,0 +1,24 @@
+package problema02;
+
+public class Problema02 {
+
+    public static void main(String[] args) {
+        int contador = 1;
+        int limite = 10;
+        int numero = 2;
+        int incremento = 4;
+        String cadena = "";
+
+        while (contador <= limite) {
+
+            numero = numero + incremento;
+            incremento = incremento + 2;
+            contador = contador + 1;
+            cadena = String.format("%s\n", numero);
+            System.out.printf("%s", cadena);
+
+        }
+
+    }
+
+}
